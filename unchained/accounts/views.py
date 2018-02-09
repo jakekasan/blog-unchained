ac85b61454c3622f54ac11eba35ac68cdb05215a
jakekasan <jake.kasan@gmail.com>
2018-02-09 12:21:17 +0000

    basic functionality completed

--- unchained/accounts/views.py
+++ unchained/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout
 # Create your views here.
 
 def signup_view(request):
@@ -7,7 +8,8 @@ def signup_view(request):
         # new user
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request,user)
             # log the user in
             return(redirect('articles:list'))
     else:
@@ -20,8 +22,19 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             # perform login
-            return(redirect('articles:list'))
+            user = form.get_user()
+            login(request,user)
+            if 'next' in request.POST:
+                return(redirect(request.POST.get("next")))
+            else:
+                return(redirect('articles:list'))
     else:
         # give login prompt
         form = AuthenticationForm()
     return(render(request,'accounts/login.html',{'form':form}))
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return(redirect('articles:list'))
+    pass
--- unchained/articles/models.py
+++ unchained/articles/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Article(models.Model):
@@ -8,6 +8,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png',blank=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE ,default=None)
     # add author later
 
     def __str__(self):
